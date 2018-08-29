5b5a8fb0867fde28f9ee53ba4f586f27286ae2a3
Connor Riva <criva@westmont.edu>
2018-08-28 23:48:07 -0700

    Move script back, mark with defer.

--- js/layout.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $(".projects").css("width","99%")
-    window.setTimeout(back_to_normal,1000);
-});
-
-function back_to_normal() {
-    $(".projects").css("width","100%")
-    $(window).trigger('resize');
-}
